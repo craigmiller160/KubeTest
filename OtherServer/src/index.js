00db3144b5cc7f3bfcf0b92cfe96b8a6672b447a
Craig Miller <craigmiller160@gmail.com>
2020-06-13 11:23:23 -0400

    First draft of server config

---
+++ OtherServer/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.get('/test', async (req, res) => {
+    console.log('Test called');
+    const ajaxRes = await axios.get('http://localhost:3000/hello');
+    res.send(`Ajax Response: ${ajaxRes.data}`);
+});
+
+app.get('/healthcheck', (req, res) => res.send('Healthy'));
+
+app.listen(3001, () => console.log('Running'));
