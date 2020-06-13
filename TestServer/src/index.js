254517c1e2e87910c6b2bb8d52168d5dcbe64122
Craig Miller <craigmiller160@gmail.com>
2020-06-13 09:56:01 -0400

    Added a healthcheck to testserver

--- TestServer/src/index.js
+++ TestServer/src/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.get('/hello', (req, res) => res.send('Hello World'));
+app.get('/hello', (req, res) => {
+    console.log('Hello Called');
+    res.send('Hello World');
+});
+
+app.get('/healthcheck', (req, res) => {
+    console.log('Healthcheck called');
+    res.send('Healthy');
+});
 
 app.listen(3000, () => console.log('Running'));
